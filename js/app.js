41154eb3e514d9f1c6fd355bc775d401706f4cba
ulaku06-rgb <ulaku06@gmail.com>
2026-09-01 21:44:21 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-let user = JSON.parse(localStorage.getItem("user"));
+window.onload = function(){
 
-if(!user){
-    window.location.href="index.html";
-}"
+const user = localStorage.getItem("user");
+
+if(user){
+
+const data = JSON.parse(user);
+
+const login = document.getElementById("login");
+const app = document.getElementById("app");
+const welcome = document.getElementById("welcome");
+
+if(login) login.style.display="none";
+
+if(app) app.style.display="block";
+
+if(welcome)
+welcome.innerHTML = "Welcome " + data.name;
+
+}
+
+}
