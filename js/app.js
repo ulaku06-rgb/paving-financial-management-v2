c59383e0601f9b18a0a1b90df7f3644acb8754b2
ulaku06-rgb <ulaku06@gmail.com>
2026-09-02 07:53:36 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,139 @@
-window.onload = function(){
+const user = JSON.parse(localStorage.getItem("user"));
 
-const user = localStorage.getItem("user");
+if(!user){
 
-if(user){
+document.body.innerHTML = `
+<h1>PPFMS Login</h1>
 
-const data = JSON.parse(user);
+<input id="username" placeholder="Username">
 
-const login = document.getElementById("login");
-const app = document.getElementById("app");
-const welcome = document.getElementById("welcome");
+<input id="password" 
+type="password" 
+placeholder="Password">
 
-if(login) login.style.display="none";
+<button onclick="login()">
+Login
+</button>
 
-if(app) app.style.display="block";
+<p id="msg"></p>
+`;
 
-if(welcome)
-welcome.innerHTML = "Welcome " + data.name;
+}else{
+
+
+document.body.innerHTML = `
+
+<h1>PPFMS v2</h1>
+
+<h2>
+Welcome ${user.name}
+</h2>
+
+<hr>
+
+
+<div class="dashboard">
+
+
+<div>
+<h3>RAB</h3>
+<h2 id="rab">
+Rp 0
+</h2>
+</div>
+
+
+<div>
+<h3>Biaya</h3>
+<h2 id="biaya">
+Rp 0
+</h2>
+</div>
+
+
+<div>
+<h3>Pendapatan</h3>
+<h2 id="income">
+Rp 0
+</h2>
+</div>
+
+
+<div>
+<h3>Profit</h3>
+<h2 id="profit">
+Rp 0
+</h2>
+</div>
+
+
+</div>
+
+
+<br>
+
+
+<button onclick="logout()">
+Logout
+</button>
+
+
+`;
 
 }
+
+
+
+async function login(){
+
+
+let username =
+document.getElementById("username").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+let result =
+await apiLogin(
+username,
+password
+);
+
+
+
+if(result.user){
+
+
+localStorage.setItem(
+"user",
+JSON.stringify(result.user)
+);
+
+
+location.reload();
+
+
+}else{
+
+
+document.getElementById("msg").innerHTML =
+"Login gagal";
+
+
+}
+
+
+}
+
+
+
+function logout(){
+
+localStorage.removeItem("user");
+
+location.reload();
 
 }
