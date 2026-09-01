121f6feb5d0ac79bf8c371deb6d11f02067c243f
ulaku06-rgb <ulaku06@gmail.com>
2026-09-01 20:48:03 +0800

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,1 +1,50 @@
-const API_URL='https://YOUR-WORKER.workers.dev';
+const API_URL = "https://ppfms-api.ulaku06.workers.dev";
+
+
+async function apiLogin(username,password){
+
+    const res = await fetch(
+        `${API_URL}/api/login`,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                username,
+                password
+            })
+        }
+    );
+
+    return await res.json();
+}
+
+
+async function getExpenses(){
+
+    const res = await fetch(
+        `${API_URL}/api/expenses`
+    );
+
+    return await res.json();
+
+}
+
+
+async function addExpense(data){
+
+    const res = await fetch(
+        `${API_URL}/api/expenses`,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+        }
+    );
+
+    return await res.json();
+
+}
